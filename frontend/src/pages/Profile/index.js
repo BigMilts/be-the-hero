fdf598cd3703f9b672338eaacce6178c74d4ec35
BigMilts <miltonvieirasoutomaior09@outlook.com>
2020-03-29 15:39:23 -0300

    mobile of application done v1.0

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,4 +1,4 @@
-import React, {  useState, useEffect }from 'react';
+import React, { useState, useEffect }from 'react';
 import { Link, useHistory} from 'react-router-dom';
 import logoImg from '../../assets/logo.svg'
 import { FiPower, FiTrash2 } from 'react-icons/fi'
